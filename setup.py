8d17355f7adfd891ea5831af95eaba2b0c5aa85b
Carey Li <hello@carey.li>
2019-03-29 12:59:13 +0000

    Make distribution ready

--- setup.py
+++ setup.py
@@ -2,13 +2,20 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="flask-core",
     version="0.0.1",
     author="Carey Li, Sean Yeoh, Zain Afzal",
     author_email="cs6443@cse.unsw.edu.au",
+    description="A modular Flask core for CTF war-games, originally created for CS6443",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["flask", "psycopg2", "psycopg2-binary", "SQLAlchemy", "cryptography"],
     package_data={"flask-core": ["templates/*/*"]},
+    classifiers=["Programming Language :: Python :: 3"],
 )
